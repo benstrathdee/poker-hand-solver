7fa48fdb6341c9935bfa779f3950afda9a9462a7
Ben Strathdee <ben.strathdee@live.com.au>
2022-01-28 09:36:35 +1100

    added my testing class, limited as it may be

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MainTest {
     @Test
@@ -21,11 +21,28 @@ public class MainTest {
         assertEquals(4, poker.players.get(0).victories);
     }
 //
+    @Test
+    public void playerTwoShouldWin() throws InvalidGameString {
+        byte playerCount = 2;
+        byte handSize = 5;
+        PokerGame poker = new PokerGame(playerCount, handSize);
+        poker.playRound("7H AS JD 6S 7S QS 3C 5H QH 8S");
+        assertEquals(1, poker.players.get(1).victories);
+        poker.playRound("7H AS JD 6S 7S QS QC QH QH 8S");
+        assertEquals(2, poker.players.get(1).victories);
+        poker.playRound("5S 6C 9S 8D 2H TH 8H 7S QS 7D");
+        assertEquals(3, poker.players.get(1).victories);
+        poker.playRound("7C 6D 9D 3H 6C QC 2C QS AS 8C");
+        assertEquals(4, poker.players.get(1).victories);
+    }
+
 //    @Test
 //    public void shouldThrowError() {
-//        byte playerCount = 3;
+//        byte playerCount = 2;
 //        byte handSize = 5;
 //        PokerGame poker = new PokerGame(playerCount, handSize);
-//        assertTh
+//        InvalidGameString thrown = Assert.assertThrows(InvalidGameString.class, poker.playRound("assdfsdfsdf"));
+//        Assert.ass(poker.playRound("assdfsdfsdf"));
+//
 //    }
 }
